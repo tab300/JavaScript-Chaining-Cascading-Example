8540ad6d3d0837e9916028896ada979e7918ae73
Tola Abiola <leston_1@hotmail.com>
2017-09-15 09:26:59 -0600

    minor changes

--- chaining.js
+++ chaining.js
@@ -1,13 +1,14 @@
 // Assing Object.prototype to variable obj
 var obj = Object.prototype;
 
-// Make custom method "makeBig"
+// Make custom method "reverseString"
 obj.reverseString = function(){
   var k = this.split("");
   var j = k.reverse();
   return String(j).replace(/,/g, "");
 }
 
+// Make a custom method "capitalize"
 obj.capitalize = function(){
   var l = this.toLowerCase();
   var k = l.split("");
