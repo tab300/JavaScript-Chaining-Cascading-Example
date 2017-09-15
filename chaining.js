41876ad05158c244fd806f74e67e8e0b1c853e16
Tola Abiola <leston_1@hotmail.com>
2017-09-15 09:52:13 -0600

    minor changes

--- chaining.js
+++ chaining.js
@@ -3,28 +3,37 @@ var obj = Object.prototype;
 
 // Make custom method "reverseString"
 obj.reverseString = function(){
+  // Check to see if the given string is empty or not
   if(String(this) === ""){
     return "Error: empty string.";
   }else{
-    var stringToArray = this.split(""); // Turn the string into an array
-    var reversedArray = stringToArray.reverse();    // Use the reverse array method to flip the contents of the string
-    return String(reversedArray).replace(/,/g, ""); // Remove all the commas and return the new string
+    // Turn the string into an array
+    var stringToArray = this.split("");
+    // Use the reverse array method to flip the contents of the string
+    var reversedArray = stringToArray.reverse();
+    // Remove all the commas and return the new string
+    return String(reversedArray).replace(/,/g, "");
   }
 }
 
 
 // Make a custom method "capitalize"
 obj.capitalize = function(){
+  // Check to see if the given string is empty or not
   if(String(this) === ""){
     return "Error: empty string.";
   }else{
+    // Turn the given string into all lowercase first
     var entireStringToLower = this.toLowerCase();
+    // Turn the new lowercased string into an array
     var stringIntoArray = entireStringToLower.split("");
+    // Change the element in the [0] index into uppercase
     stringIntoArray[0] = stringIntoArray[0].toUpperCase();
+    // Remove all the commas and converting the casting the array into a string
     return String(stringIntoArray).replace(/,/g, "");
   }
 }
 
-
+// Test-drive
 console.log("".capitalize());
 console.log("".reverseString().capitalize());
