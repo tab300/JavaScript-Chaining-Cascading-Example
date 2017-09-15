8ed1641cf71a6cc237b8c10bd61150475bc85d3e
Tola Abiola <leston_1@hotmail.com>
2017-09-15 09:23:58 -0600

    minor changes

--- chaining.js
+++ chaining.js
@@ -2,29 +2,20 @@
 var obj = Object.prototype;
 
 // Make custom method "makeBig"
-obj.makeBig = function(){
-  return this.toUpperCase();
+obj.reverseString = function(){
+  var k = this.split("");
+  var j = k.reverse();
+  return String(j).replace(/,/g, "");
 }
 
-// Make custom method "reverse"
-obj.reverse = function(){
-  var objArray = [];
-  var words = this;
-  for(var i = 1 - words.length; i < 1; i++){
-     var ea = words[-i];
-     objArray.push(ea);
-  }
-
-  return String(objArray).replace(/,/g, "");
+obj.capitalize = function(){
+  var l = this.toLowerCase();
+  var k = l.split("");
+  k[0] = k[0].toUpperCase();
+  return String(k).replace(/,/g, "");
 }
 
-// Make custom method "makeCount"
-obj.makeCount = function(){
-  return this.length;
-}
 
-// Test-drive the cascading
-console.log("Hiyafgrss".makeBig()
-                       .reverse()
-                       .makeCount()
-           );    // 9
+
+console.log("MaDmAn".reverseString().capitalize());
+console.log("DisaPPear".capitalize());
